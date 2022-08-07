c7e2c70ad13345a11c8a5dd4bd595ba454c16b6e
sn <me@stasn.ru>
2022-08-07 21:28:25 +0800

    update;

--- src/app.tsx
+++ src/app.tsx
@@ -73,7 +73,7 @@ function App() {
             </span>
           </h1>
           <div className="text-xl md:text-2xl pt-3 font-light text-gray-400 text-center">
-            Data engineer and backend developer.
+            Backend & blockchain developer.
           </div>
           <div className="text-xl pt-10 md:text-2xl font-light text-gray-700 flex flex-col md:flex-row md:space-x-5 space-y-2 md:space-y-0 items-center justify-center break-before-all">
             <Link to="https://t.me/stasnepom">telegram</Link>
