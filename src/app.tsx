21d6d22fb9c8335777c45a52d704ac335957139a
sn <me@stasn.ru>
2022-08-17 15:19:36 +0800

    update;

--- src/app.tsx
+++ src/app.tsx
@@ -80,7 +80,7 @@ function App() {
             </span>
           </h1>
           <div className="text-xl md:text-2xl pt-3 font-light text-gray-400 text-center">
-            Developer of impressive things and just a curious person.
+            Blockchain developer.
           </div>
           <div className="text-xl pt-10 md:text-2xl font-light text-gray-700 flex flex-col md:flex-row md:space-x-5 space-y-2 md:space-y-0 items-center justify-center break-before-all">
             <Link to="https://blog.stasn.me/">blog</Link>
