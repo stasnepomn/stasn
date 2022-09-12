a1e87f1c7632a88b26868d5856967d894c373a7a
sn <me@stasn.ru>
2022-09-12 14:22:17 +0800

    update;

--- src/app.tsx
+++ src/app.tsx
@@ -59,6 +59,23 @@ export const App: FunctionComponent = () => {
             </Link>
           </animated.div>
         </animated.section>
+        <animated.div style={contentStyles} className="section-text">
+          List of my projects{' '}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="inline-block h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+            />
+          </svg>
+        </animated.div>
         <animated.section style={contentStyles} className="projects">
           {projects.map((project) => (
             <Card
