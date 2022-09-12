9c178368595367de160eb334f2ecda60c72817f0
sn <me@stasn.ru>
2022-09-12 14:05:13 +0800

    update;

--- src/projects.ts
+++ src/projects.ts
@@ -32,9 +32,10 @@ export const projects = [
   },
   {
     title: 'VelvetMind',
-    subtitle: 'coming soon',
+    subtitle: 'open-source',
     image: VelvetMind,
-    comment: 'Your personal mood diary',
+    link: 'https://velvetmind.space/',
+    comment: 'Your personal encrypted diary',
   },
   {
     title: 'Dodoer',
